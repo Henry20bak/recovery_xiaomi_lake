5a359ba7a504975f35c07c1efb9ef764451476db
Henry20bak <123889245+Henry20bak@users.noreply.github.com>
2025-05-13 19:55:49 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,11 +8,3 @@
 add_lunch_combo twrp_lake-user
 add_lunch_combo twrp_lake-userdebug
 add_lunch_combo twrp_lake-eng
-
-export FOX_VERSION="R12.1"
-export FOX_BUILD_TYPE="Beta"
-export FOX_VARIANT="A12" 
-export OF_MAINTAINER="YourName"
-# Vendorboot specific settings
-export OF_AB_DEVICE=1
-export OF_VIRTUAL_AB_DEVICE=1
