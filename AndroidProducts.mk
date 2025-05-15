4eccc3c21221c39dd236eb4f19def58a8907f669
Henry20bak <123889245+Henry20bak@users.noreply.github.com>
2025-05-15 13:15:03 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,6 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_lake.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_lake-user \
-    twrp_lake-userdebug \
-    twrp_lake-eng
+    twrp_lake-eng \
+    twrp_lake-userdebug
