02c04b5cb748f4117a962dd33630a03e3dbc18f7
Henry20bak <123889245+Henry20bak@users.noreply.github.com>
2025-05-17 23:10:56 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,35 +1,85 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 LOCAL_PATH := device/xiaomi/lake
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Enable Virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 31
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 31
+
+ENABLE_VIRTUAL_AB := true
+AB_OTA_UPDATER := true
+
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    lk \
+    odm \
+    odm_dlkm \
+    product \
+    system \
+    system_ext \
+    vbmeta_system \
+    vbmeta_vendor \
+    vendor \
+    vendor_boot \
+    vendor_dlkm
+
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+PRODUCT_PROPERTY_OVERRIDES += ro.twrp.vendor_boot=true
 
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Virtual AB
-ENABLE_VIRTUAL_AB := true
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
-
-# A/B
-AB_OTA_UPDATER := true
-AB_OTA_PARTITIONS += \
-    boot \
-    dtbo \
-    product \
-    system \
-    system_ext \
-    vbmeta \
-    vbmeta_system \
-    vbmeta_vendor \
-    vendor \
-    vendor_boot
-
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-impl.recovery \
-    android.hardware.boot@1.2-service
+    android.hardware.boot@1.2-service \
+    android.hardware.boot@1.2-mtkimpl \
+    android.hardware.boot@1.2-mtkimpl.recovery
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl \
+    update_engine_client
+ 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
 
 PRODUCT_PACKAGES += \
-    bootctrl \
-    bootctrl.recovery
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
+
+# Health Hal
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
 
 # Update engine
 PRODUCT_PACKAGES += \
@@ -37,69 +87,20 @@ PRODUCT_PACKAGES += \
     update_engine_sideload \
     update_verifier
 
-# Fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl-mock \
-    fastbootd
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
 
-# Crypto and Hardware Support
-PRODUCT_PACKAGES += \
-    qcom_decrypt \
-    qcom_decrypt_fbe \
-    android.hardware.gatekeeper@1.0 \
-    android.hardware.keymaster@4.0 \
-    android.hardware.keymaster@4.1 \
+# Additional Libraries
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+    libkeymaster41 \
+    libpuresoftkeymasterdevice \
     libion \
-    vendor.mediatek.hardware.videotelephony@1.0 \
-    android.hardware.graphics.composer@2.1-impl
+    libxml2
 
-# Host packages
-PRODUCT_HOST_PACKAGES += \
-    libandroidicu
-
-# Additional Hardware Support
-PRODUCT_PACKAGES += \
-    libhardware \
-    libhardware_legacy \
-    libion.recovery
-
-# Touch drivers
-PRODUCT_PACKAGES += \
-    mtk_tpd \
-    libinputflinger.vendor \
-    libgui.vendor \
-    libtouchservice.vendor
-
-# Crypto and Hardware Support
-PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-impl \
-    android.hardware.gatekeeper@1.0-service \
-    android.hardware.keymaster@4.1-service \
-    android.hardware.keymaster@4.1-impl
-
-# Stats and flags dependencies
-PRODUCT_PACKAGES += \
-    libstatslog \
-    libstatslog.vendor \
-    libstatslog.recovery \
-    server_configurable_flags \
-    server_configurable_flags.vendor \
-    server_configurable_flags.recovery \
-    server_configurable_flags.inputflinger
-
-# InputFlinger dependencies
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.input.stats_log=true \
-    ro.vendor.input.stats_log=true
-
-PRODUCT_PACKAGES += \
-    libinputflinger \
-    libinputflinger.recovery \
-    libinputflinger.inputflinger \
-    libinputflinger.vendor
-
-PRODUCT_PACKAGES += \
-    libgui \
-    libgui.vendor \
-    libtouchservice \
-    libtouchservice.vendor
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
